a2b1d51218daed33f11198970cb56f50bf1ebe7d
jiyeeeah <sije3757@snu.ac.kr>
2025-03-12 19:45:11 +0900

    chore: spring boot 프로젝트 세팅

---
+++ src/main/java/community/jessie_community/JessieCommunityApplication.java
@@ -0,0 +1,13 @@
+package community.jessie_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessieCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JessieCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/community/jessie_community/JessieCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package community.jessie_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JessieCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a1ed1d378ecf27677a6e24ac060c611898de567f
jiyeeeah <sije3757@snu.ac.kr>
2025-03-17 16:03:32 +0900

    chore: DB 연결을 위한 세팅
    - DB에 사용되는 ddl 보관
    - DB 연결에 필요한 라이브러리 dependency 추가
    - DB 연결에 활용 될 환경변수 추가

---
+++ sql/ddl.sql
@@ -0,0 +1,18 @@
+USE jessie_community;
+
+drop table if exists member CASCADE;
+create table member (
+    id BIGINT PRIMARY KEY,
+    name varchar(255),
+);
+
+
+drop table if exists users cascade;
+
+CREATE TABLE users (
+    user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    nickname VARCHAR(10) NOT NULL,
+    profile_img_url VARCHAR(255)
+);

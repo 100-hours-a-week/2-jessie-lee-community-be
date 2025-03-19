c117bbac208b9c4c1e7b96ee230b7efe11f9fb9e
jiyeeeah <sije3757@snu.ac.kr>
2025-03-19 16:01:45 +0900

    feat: Post 도메인 모델 구현
    - Post 테이블과 일치하는 모델 구현

---
+++ src/main/java/community/jessie_community/domain/Post.java
@@ -0,0 +1,96 @@
+package community.jessie_community.domain;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private Long id;
+    private Long user_id;
+    private String title;
+    private String content;
+    private String image_url;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    private int like_count;
+    private int view_count;
+    private int comment_count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public int getLike_count() {
+        return like_count;
+    }
+
+    public void setLike_count(int like_count) {
+        this.like_count = like_count;
+    }
+
+    public int getView_count() {
+        return view_count;
+    }
+
+    public void setView_count(int view_count) {
+        this.view_count = view_count;
+    }
+
+    public int getComment_count() {
+        return comment_count;
+    }
+
+    public void setComment_count(int comment_count) {
+        this.comment_count = comment_count;
+    }
+}

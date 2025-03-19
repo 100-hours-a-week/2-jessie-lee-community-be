7ed74f8ba00c1ade9abbaf803c680bf428e3ccd6
jiyeeeah <sije3757@snu.ac.kr>
2025-03-19 16:15:28 +0900

    feat: UserController에 id로 유저 한명만 불러오는 기능 구현

--- src/main/java/community/jessie_community/controller/UserController.java
+++ src/main/java/community/jessie_community/controller/UserController.java
@@ -3,11 +3,15 @@ package community.jessie_community.controller;
 import community.jessie_community.domain.User;
 import community.jessie_community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -25,4 +29,13 @@ public class UserController {
         return userService.findUsers();
     }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+        Optional<User> user = userService.findOne(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }

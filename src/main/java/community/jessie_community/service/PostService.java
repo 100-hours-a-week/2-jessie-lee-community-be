d6f1902014d7ed4cd26c56cbc9f3036aa76eb4a8
jiyeeeah <sije3757@snu.ac.kr>
2025-03-19 16:03:18 +0900

    feat: Post Service 계층 구현
    - Post repository를 활용할 수 있는 Service 계층 구현

---
+++ src/main/java/community/jessie_community/service/PostService.java
@@ -0,0 +1,37 @@
+package community.jessie_community.service;
+
+import community.jessie_community.domain.Post;
+import community.jessie_community.repository.PostRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    /**
+     * 게시글 등록
+     */
+    public Long post(Post post, Long user_id) {
+        postRepository.save(post, user_id);
+        return post.getId();
+    }
+
+    /**
+     * 전체 게시글 조회
+     */
+    public List<Post> findPosts() {
+        return postRepository.findAll();
+    }
+
+    /**
+     * id로 게시글 하나 조회
+     */
+    public Optional<Post> findOne(Long id) {
+        return postRepository.findById(id);
+    }
+}
